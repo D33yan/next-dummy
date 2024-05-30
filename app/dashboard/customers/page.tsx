ad8e923a052756f3b41ab7cc1a3c7037c86796b9
D33yan <nnajidivine2006@gmail.com>
2024-05-30 15:30:43 +0100

    first commit

---
+++ app/dashboard/customers/page.tsx
@@ -0,0 +1,7 @@
+export default function Customers(){
+    return(
+        <div>
+            <p>This is the Customers Page</p>
+        </div>
+    )
+}
---
+++ app/dashboard/invoices/page.tsx
@@ -0,0 +1,7 @@
+export default function Invoices(){
+    return(
+        <div>
+            <p>This is the Invoices Page</p>
+        </div>
+    )
+}
